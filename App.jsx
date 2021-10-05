05dcca89f6a0097d73b650dd4139b39fe39726de
kazumasa komatsu <yinbanben@gmail.com>
2021-10-05 09:56:03 +0900

    Create Hello Component

--- App.jsx
+++ App.jsx
@@ -2,9 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './src/components/Hello';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang style={{ backgroundColor: 'red' }}>World</Hello>
+      <Hello bang bar={{ fontSize: 10 }}>World</Hello>
+      <Hello bang style={{ color: 'black' }}>World</Hello>
       <Text>Open up App.js to start working on your app!</Text>
       {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import {
+  string, bool, shape, number,
+} from 'prop-types';
 
-function Hello() {
-	return (
-
-	)
+function Hello(props) {
+  const {
+    children, bang, style, foo, bar,
+  } = props;
+  return (
+    <View>
+      <Text style={[styles.text, style, foo, bar]}>
+        {`Hello ${children}${bang ? '!' : ''}`}
+      </Text>
+    </View>
+  );
 }
+
+Hello.propTypes = {
+  children: string.isRequired,
+  bang: bool,
+  style: shape(),
+  foo: shape({
+    fontSize: number,
+  }),
+  bar: shape(),
+};
+
+Hello.defaultProps = {
+  bang: false,
+  style: null,
+  foo: null,
+  bar: null,
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'white',
+    backgroundColor: 'blue',
+    fontSize: 40,
+    fontWeight: 'bold',
+    padding: 16,
+  },
+});
+
+export default Hello;
